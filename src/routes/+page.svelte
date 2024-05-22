8791d1075ec717e20d3c6a00ad7c00c0e9b534a9
dalvarez <dalvarez@intelix.biz>
2024-05-22 16:51:50 -0400

    Add banner

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<!-- <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
+<div class="bg-gradient-to-r from-blue-500 to-purple-500 text-white py-4 md:mt-16 md:py-6 lg:py-8">
+	<div class="container mx-auto flex flex-col md:flex-row items-center justify-between">
+		<img
+			src="https://plus.unsplash.com/premium_photo-1661832711795-a1cff19f9706?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+			alt="Imagen"
+			class="w-full md:w-1/2 md:mr-4 lg:mr-8 mb-4 md:mb-0"
+		/>
+		<div class="text-center md:text-left">
+			<h2 class="text-2xl md:text-3xl lg:text-4xl font-bold mb-4">¡Descuento Especial!</h2>
+			<p class="text-sm md:text-base lg:text-lg mb-4">
+				No te pierdas nuestras ofertas exclusivas. ¡Aprovecha ahora!
+			</p>
+			<button class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
+				>Sigue Viendo</button
+			>
+		</div>
+	</div>
+</div>
